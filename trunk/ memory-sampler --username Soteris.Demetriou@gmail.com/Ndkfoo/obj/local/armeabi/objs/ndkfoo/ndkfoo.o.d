4597823875b91797f81b4d8ba6c77900df5a8baa
Soteris.Demetriou@gmail.com <Soteris.Demetriou@gmail.com@823ad53e-23b4-8298-f0e4-484447987085>
2012-10-12 18:05:26 +0000

    Testing Android NDK Hello Worlds from NDK

---
+++ trunk/ memory-sampler --username Soteris.Demetriou@gmail.com/Ndkfoo/obj/local/armeabi/objs/ndkfoo/ndkfoo.o.d
@@ -0,0 +1,16 @@
+obj/local/armeabi/objs/ndkfoo/ndkfoo.o: jni/ndkfoo.c \
+ /cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h \
+ /cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h
+
+/cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h:
+
+/cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/cygdrive/c/Users/sdemetr2/android-ndk-r8b/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h:
